7870afe1736d76d2f8bd7c312d51a661c9f7385e
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-06-26 16:05:43 +0530

    Time: 3 ms (62.68%), Space: 19.4 MB (25.82%) - LeetHub - Time: O(n), Space: O(n)

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -8,18 +8,16 @@ class Solution:
         # print(f'index: {index}, dp: {dp}')
         if index >= len(cost) - 1:
             return 0
-        
+
         if dp[index] is not None:
             return dp[index]
-        
-        dp[index] =  min(
+
+        dp[index] = min(
             (cost[index] + self.recursion(index + 1, cost, dp)),
             (cost[index + 1] + self.recursion(index + 2, cost, dp)),
         )
         # print(dp, index)
         return dp[index]
-
-
 
 
 # Synced seamlessly with LeetHub Pro
